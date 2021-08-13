2389111b835234b7fe09ffd8a09983a834e24c1f
SuperHuangXu <www.1567891@qq.com>
2021-08-13 18:38:02 +0800

    feat: 🎸 适配 windows 和 Linux

--- dingding.mjs
+++ dingding.mjs
@@ -3,4 +3,20 @@ console.log(chalk.blue('钉钉内网穿透'))
 const subdomain = await question('请指定子域名(default: kittenbot)：')
 const port = await question('请指定端口号(default: 3000)：')
 
-await $`./pierced/mac_64/ding -config=./pierced/mac_64/ding.cfg -subdomain=${subdomain ? subdomain : 'kittenbot'} ${port ? port : 3000}`
+let piercedPath = ''
+let cfgPath = ''
+
+if (os.platform() === 'win32') {
+  piercedPath = 'windows_64/ding.exe'
+  cfgPath = 'windows_64/ding.cfg'
+}
+if (os.platform() === 'darwin') {
+  piercedPath = 'mac_64/ding'
+  cfgPath = 'mac_64/ding.cfg'
+}
+if (os.platform() === 'linux') {
+  piercedPath = 'linux/ding'
+  cfgPath = 'linux/ding.cfg'
+}
+
+await $`./pierced/${piercedPath} -config=./pierced/${cfgPath} -subdomain=${subdomain ? subdomain : 'kittenbot'} ${port ? port : 3000}`
