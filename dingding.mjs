a2f9394a1f1a57d472874c569da3e91265cbd931
SuperHuangXu <www.1567891@qq.com>
2021-08-13 19:04:23 +0800

    fix: 🐛 修复windows

--- dingding.mjs
+++ dingding.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 console.log(chalk.blue('钉钉内网穿透'))
-const subdomain = await question('请指定子域名(default: kittenbot)：')
+let subdomain = await question('请指定子域名(default: kittenbot)：')
 const port = await question('请指定端口号(default: 3000)：')
 
 let piercedPath = ''
@@ -8,15 +8,16 @@ let cfgPath = ''
 
 if (os.platform() === 'win32') {
   piercedPath = 'windows_64/ding.exe'
-  cfgPath = 'windows_64/ding.cfg'
+  cfgPath = '"./pierced/windows_64/ding.cfg"'
+  subdomain = '"' + subdomain + '"'
 }
 if (os.platform() === 'darwin') {
   piercedPath = 'mac_64/ding'
-  cfgPath = 'mac_64/ding.cfg'
+  cfgPath = './pierced/mac_64/ding.cfg'
 }
 if (os.platform() === 'linux') {
   piercedPath = 'linux/ding'
-  cfgPath = 'linux/ding.cfg'
+  cfgPath = './pierced/linux/ding.cfg'
 }
 
-await $`./pierced/${piercedPath} -config=./pierced/${cfgPath} -subdomain=${subdomain ? subdomain : 'kittenbot'} ${port ? port : 3000}`
+await $`./pierced/${piercedPath} -config=${cfgPath} -subdomain=${subdomain ? subdomain : 'kittenbot'} ${port ? port : 3000}`
